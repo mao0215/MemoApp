76bb7ca2c706f14d6477e69b63d514d16c8a583c
mao <111213096+mao0215@ users.noreply.github.com>
2025-06-25 13:58:34 +0900

    memo list , header style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,20 +4,21 @@ import { View, Text, StyleSheet } from "react-native";
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        {/*ヘッダーの下側 */}
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+      {/*ヘッダーの下側 */}
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
-        {/*ヘッダーの下側 */}
       </View>
+      {/*ヘッダーの下側 */}
+      {/*memoList*/}
       <View>
         {/* memoItem1の構造体 */}
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2025年6月25日 10:00</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2025年6月25日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -26,21 +27,22 @@ const Index = (): JSX.Element => {
         {/* memoItem1の構造体 */}
 
         {/* memoItem2の構造体 */}
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2025年6月25日 10:00</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2025年6月25日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
         {/* memoItem2の構造体 */}
+
         {/* memoItem3の構造体 */}
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2025年6月25日 10:00</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2025年6月25日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -48,8 +50,8 @@ const Index = (): JSX.Element => {
         </View>
         {/* memoItem3の構造体 */}
       </View>
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   );
@@ -58,8 +60,70 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  header: {
+    backgroundColor: "#467FD3",
+    height: 104,
+    justifyContent: "flex-end",
+  },
+  headerInner: {
+    alignItems: "center",
+  },
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: "rgba(255,255,255,0.7)",
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  memoListItem: {
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgba(0,0,0,0.15)",
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484",
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#467FD3",
     justifyContent: "center",
     alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    //shadowはiphoneのみ有効
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    //android用
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 48,
   },
 });
 export default Index;
