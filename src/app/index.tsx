7ae1fbe97702b3bf1827b7f62d29ea6053152036
mao <111213096+mao0215@ users.noreply.github.com>
2025-06-25 12:03:09 +0900

    Add Styel for memoList

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,65 @@
 import { JSX } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World !</Text>
+    <View style={styles.container}>
+      <View>
+        {/*ヘッダーの下側 */}
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+        {/*ヘッダーの下側 */}
+      </View>
+      <View>
+        {/* memoItem1の構造体 */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年6月25日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* memoItem1の構造体 */}
+
+        {/* memoItem2の構造体 */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年6月25日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* memoItem2の構造体 */}
+        {/* memoItem3の構造体 */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年6月25日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* memoItem3の構造体 */}
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 export default Index;
