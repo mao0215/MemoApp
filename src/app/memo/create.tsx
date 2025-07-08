a2fb45705ab6d97739bbaf98cb15e2aa49703bad
mao <111213096+mao0215@ users.noreply.github.com>
2025-07-08 12:04:43 +0900

    データベースのデータをメモに反映させた

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,30 +1,55 @@
-import { JSX } from "react";
-import {
-  View,
-  TextInput,
-  StyleSheet,
-  KeyboardAvoidingView,
-} from "react-native";
+import { JSX, useState } from "react";
+import { View, TextInput, StyleSheet } from "react-native";
 import { router } from "expo-router";
-import { collection, addDoc } from "firebase/firestore";
-import { db } from "../../config";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
+import { db, auth } from "../../config";
 
-import Header from "../../components/header";
+//import Header from "../../components/header";
 import CircleButton from "../../components/circleButton";
 import Icon from "../../components/icon";
-
-const handlePress = (): void => {
-  router.back();
+import KeyboardAvoidingView from "../../components/KeyboardAvoidingView";
+const handlePress = (bodyText: string): void => {
+  //新規メモを作成しデータベースへ保存
+  if (auth.currentUser === null) {
+    return;
+  }
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`); //usersの中の今のidの中のmemosの中に格納
+  addDoc(ref, {
+    //子の中が保存したいデータ
+    bodyText: bodyText, //memonの中身
+    updatedAt: Timestamp.fromDate(new Date()), //時間の中身
+  })
+    .then((docRef) => {
+      //docRefドキュメントへの参照
+      console.log("success", docRef.id);
+      router.back();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const Create = (): JSX.Element => {
+  const [bodyText, setBodyText] = useState("");
   return (
-    <KeyboardAvoidingView behavior="height" style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       {/* <Header /> */}
       <View style={styles.inputContiner}>
-        <TextInput multiline style={styles.input} value="" />
+        <TextInput
+          multiline
+          style={styles.input}
+          value={bodyText}
+          onChangeText={(text) => {
+            setBodyText(text);
+          }}
+          autoFocus
+        />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton
+        onPress={() => {
+          handlePress(bodyText);
+        }}
+      >
         <Icon name="check" size={40} color="#ffffff" />
       </CircleButton>
     </KeyboardAvoidingView>
--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,7 +1,8 @@
 import { JSX } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, unstable_batchedUpdates } from "react-native";
 import { router, useNavigation } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 
 import Header from "../../components/header";
 import MemoListItem from "../../components/MemoListItem";
@@ -10,29 +11,58 @@ import CircleButton from "../../components/circleButton";
 import Icon from "../../components/icon";
 import LogOutButton from "../../components/logoutbutton";
 
+import { db, auth } from "../../config";
+import { type Memo } from "../../../types/memo";
+
 const handlePress = (): void => {
   router.push("/memo/create");
 };
 
-const Index = (): JSX.Element => {
+const List = (): JSX.Element => {
+  const [memos, setMemos] = useState<Memo[]>([]);
   const navigation = useNavigation();
   useEffect(() => {
+    //useEffectは画面が表示するときと、消える時の処理に使える
     navigation.setOptions({
       headerRight: () => {
         return <LogOutButton />;
       },
     });
   }, []);
-
+  useEffect(() => {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+    const q = query(ref, orderBy("updatedAt", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = [];
+      snapshot.forEach((doc) => {
+        console.log("memo", doc.data()); //doc.data()でメモデータにアクセス
+        const { bodyText, updatedAt } = doc.data();
+        remoteMemos.push({
+          //remoteMomos配列に入れる
+          id: doc.id,
+          bodyText,
+          updatedAt,
+        });
+      });
+      setMemos(remoteMemos);
+    });
+    return unsubscribe; //上のunsubscribeの関数が実行されるのは、画面が消えたタイミング
+  }, []);
   return (
     <View style={styles.container}>
       {/* <Header /> */}
 
       {/*memoList*/}
       <View>
+        {memos.map((memo) => (
+          <MemoListItem memo={memo} /> //{retrun  <MemoListItem memo={memo} />}と同じ意味
+        ))}
+        {/* <MemoListItem />
         <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
+        <MemoListItem /> */}
       </View>
 
       <CircleButton onPress={handlePress}>
@@ -49,4 +79,4 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
   },
 });
-export default Index;
+export default List;
